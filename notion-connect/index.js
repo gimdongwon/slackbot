f2f46da9339d0913de594246936e16f2c70c8ab1
gimdongwon <ehddnjs8989@gmail.com>
2024-04-09 00:54:21 +0900

    notion post 성공

--- notion-connect/index.js
+++ notion-connect/index.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const { getCalendarList, postCalendar } = require('./services/notion')
-const PORT = process.env.PORT || 5001
+const express = require("express");
+const { getCalendarList, postCalendar } = require("./services/notion");
+const PORT = process.env.PORT || 5001;
 
-const app = express()
+const app = express();
 
 /**
  * 노션 캘린더 데이터 가져오기
  */
-app.get('/calendars', async(req, res) => {
-    const calendars = await getCalendarList()
-    res.json(calendars);
-})
+app.get("/calendars", async (req, res) => {
+  const calendars = await getCalendarList();
+  res.json(calendars);
+});
 
-app.post('/calendar', async (req, res) => {
-    const result = await postCalendar();
-    res.json(result);
-})
+app.post("/calendar", async (req, res) => {
+  const result = await postCalendar(req.text);
+  res.json(result);
+});
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`))
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
--- notion-connect/services/notion.js
+++ notion-connect/services/notion.js
@@ -1,43 +1,48 @@
 // services > notion.js를 추가하여 입력합니다.
 
-const dotenv = require('dotenv').config()
-const { Client } = require('@notionhq/client')
+const dotenv = require("dotenv").config();
+const { Client } = require("@notionhq/client");
 
 //기존 Postman에서 Authorization의 Bearer Token 설정한 것과 동일한 의미입니다.
 const notion = new Client({
-    auth: process.env.NOTION_TOKEN
-})
-const database_id = process.env.NOTION_DATABASE_ID
+  auth: process.env.NOTION_TOKEN,
+});
+const database_id = process.env.NOTION_DATABASE_ID;
 
 module.exports.getCalendarList = async function getCalendarList() {
-    const { results } = await notion.databases.query({
-        database_id: database_id
-	})
-	const calendar = results.map((page) => {
-		return {
-			id: page?.id,
-			title: page?.properties?.Name?.title[0]?.text?.content,
-			gender: page?.properties?.Gender?.multi_select[0]?.name,
-			age: page?.properties?.Age?.number,
-		}
-	});
-	return calendar
-}
+  const { results } = await notion.databases.query({
+    database_id: database_id,
+  });
+  const calendar = results.map((page) => {
+    return {
+      id: page?.id,
+      title: page?.properties?.Name?.title[0]?.text?.content,
+      gender: page?.properties?.Gender?.multi_select[0]?.name,
+      age: page?.properties?.Age?.number,
+    };
+  });
+  return calendar;
+};
 
-module.exports.postCalendar = async function postCalendar() {
-	// await notion.databases.create({
-	// 	parent: { database_id },
-	// 	properties: {
-	// 		title: {
-	// 			title: [
-	// 				{
-	// 					"text": {
-	// 						"content": "test"
-	// 					}
-	// 				}
-	// 			]
-	// 		}
-	// 	}
-	// })
-	return "success"
-}
+module.exports.postCalendar = async function postCalendar(text) {
+  try {
+    const response = await notion.pages.create({
+      parent: { database_id },
+      properties: {
+        title: {
+          rich_text: [
+            {
+              text: {
+                content: "yeonwoo",
+              },
+            },
+          ],
+        },
+      },
+    });
+    console.log(response);
+    console.log("Success! Entry added.");
+  } catch (error) {
+    console.error(error.body);
+  }
+};
