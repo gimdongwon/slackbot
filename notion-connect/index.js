1f706d4768dfd93454c70a1dea278d7b92b34314
gimdongwon <ehddnjs8989@gmail.com>
2024-04-10 14:52:45 +0900

    nodemon age 추가

--- notion-connect/index.js
+++ notion-connect/index.js
@@ -4,16 +4,19 @@ const PORT = process.env.PORT || 5001;
 
 const app = express();
 
+app.use(express.json());
+
 /**
  * 노션 캘린더 데이터 가져오기
  */
+
 app.get("/calendars", async (req, res) => {
   const calendars = await getCalendarList();
   res.json(calendars);
 });
 
 app.post("/calendar", async (req, res) => {
-  const result = await postCalendar(req.text);
+  const result = await postCalendar(req.body);
   res.json(result);
 });
 
--- notion-connect/services/notion.js
+++ notion-connect/services/notion.js
@@ -11,38 +11,52 @@ const database_id = process.env.NOTION_DATABASE_ID;
 
 module.exports.getCalendarList = async function getCalendarList() {
   const { results } = await notion.databases.query({
-    database_id: database_id,
+    database_id,
   });
   const calendar = results.map((page) => {
     return {
       id: page?.id,
       title: page?.properties?.Name?.title[0]?.text?.content,
-      gender: page?.properties?.Gender?.multi_select[0]?.name,
+      gender: page?.properties?.Gender?.multi_select?.[0]?.name,
       age: page?.properties?.Age?.number,
     };
   });
   return calendar;
 };
 
-module.exports.postCalendar = async function postCalendar(text) {
+module.exports.postCalendar = async function postCalendar(req) {
   try {
-    const response = await notion.pages.create({
-      parent: { database_id },
-      properties: {
-        title: {
-          rich_text: [
-            {
-              text: {
-                content: "yeonwoo",
-              },
+    const properties = {
+      title: {
+        title: [
+          {
+            type: "text",
+            text: {
+              content: req.text,
             },
-          ],
-        },
+          },
+        ],
       },
+      Gender: {
+        rich_text: [
+          {
+            text: {
+              content: req.gender,
+            },
+          },
+        ],
+      },
+      Age: {
+        number: req.age,
+      },
+    };
+    const result = await notion.pages.create({
+      parent: { database_id },
+      properties,
     });
-    console.log(response);
     console.log("Success! Entry added.");
+    return result;
   } catch (error) {
-    console.error(error.body);
+    console.error(error, "error");
   }
 };
