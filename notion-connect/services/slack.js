a0c037335c372d02dd942518559ae6369387bfe5
gimdongwon <ehddnjs8989@gmail.com>
2024-04-17 01:01:22 +0900

    slack postMessage 호출성공

--- notion-connect/services/slack.js
+++ notion-connect/services/slack.js
@@ -1,22 +1,25 @@
-const { WebClient } = require("@slack/web-api");
+const { WebClient, LogLevel } = require("@slack/web-api");
 
 // An access token (from your Slack app or custom integration - xoxp, xoxb)
-const token = process.env.SLACK_TOKEN;
+const token = process.env.SLACK_BOT_TOKEN;
 
-const web = new WebClient(token);
+const web = new WebClient(token, {
+  logLevel: LogLevel.DEBUG,
+});
 
 // This argument can be a channel ID, a DM ID, a MPDM ID, or a group ID
-const conversationId = "C06U9GT0VH6";
+const conversationId = process.env.CONVERSATION_ID;
 
 module.exports.sendSlackMessage = async () => {
   try {
     const res = await web.chat.postMessage({
       channel: conversationId,
-      text: "Hello there",
+      text: "success!!",
     });
 
     // `res` contains information about the posted message
     console.log("Message sent: ", res.ts);
+    return res;
   } catch (error) {
     console.error(error);
   }
